c51c7312dbff3c7fdf3d15eaeb34e044f6c902c8
alexlittle <alex@alexlittle.net>
2014-02-27 23:26:10 +0100

    CLOSED - task 69: Add option to export 'feedback' activities https://github.com/DigitalCampus/moodle-block_oppia_mobile_export/issues/issues/issue/69

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014022000;
+$plugin->version = 2014022700;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
