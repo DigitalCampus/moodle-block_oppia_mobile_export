a16ab591de637fbe173019050bc50a01928561f3
alexlittle <alex@alexlittle.net>
2014-03-08 18:31:42 +0100

    CLOSED - task 71: Add export of course description https://github.com/DigitalCampus/moodle-block_oppia_mobile_export/issues/issues/issue/71

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014022800;
+$plugin->version = 2014030800;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
