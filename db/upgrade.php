f76b5d3b7d2ae1b86c6e4a37a2999002cf6232b4
alexlittle <alex@alexlittle.net>
2014-03-21 12:37:43 +0100

    OPEN - task 36: Allow separate usernames/ API keys to be entered for each user https://github.com/DigitalCampus/moodle-block_oppia_mobile_export/issues/issues/issue/36

--- db/upgrade.php
+++ db/upgrade.php
@@ -1,18 +1,44 @@
 <?php 
 
-// This file keeps track of upgrades to 
+// This file keeps track of upgrades to
 // the oppia_mobile_export block
 
 function xmldb_block_oppia_mobile_export_upgrade($oldversion) {
 
-    global $CFG, $DB, $OUTPUT;
+	global $CFG, $DB, $OUTPUT;
 
 	$dbman = $DB->get_manager();
+
+	if ($oldversion < 2013111402) {
+		// block savepoint reached
+		upgrade_block_savepoint(true, 2013111402, 'oppia_mobile_export');
+	}
+
+	if ($oldversion < 2014032100) {
 	
-    if ($oldversion < 2013111402) {
-        // block savepoint reached
-        upgrade_block_savepoint(true, 2013111402, 'oppia_mobile_export');
-    }
-     
-    return true;
+		// Define table block_oppia_mobile_server to be created.
+		$table = new xmldb_table('block_oppia_mobile_server');
+	
+		// Adding fields to table block_oppia_mobile_server.
+		$table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+		$table->add_field('servername', XMLDB_TYPE_CHAR, '50', null, null, null, '');
+		$table->add_field('url', XMLDB_TYPE_CHAR, '50', null, null, null, '');
+		$table->add_field('moodleuserid', XMLDB_TYPE_INTEGER, '10', null, null, null, '0');
+		$table->add_field('username', XMLDB_TYPE_CHAR, '50', null, null, null, '');
+		$table->add_field('apikey', XMLDB_TYPE_CHAR, '50', null, null, null, '');
+		$table->add_field('defaultserver', XMLDB_TYPE_INTEGER, '10', null, null, null, '0');
+	
+		// Adding keys to table block_oppia_mobile_server.
+		$table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+	
+		// Conditionally launch create table for block_oppia_mobile_server.
+		if (!$dbman->table_exists($table)) {
+			$dbman->create_table($table);
+		}
+	
+		// Blocks savepoint reached.
+		upgrade_plugin_savepoint(true, 2014032100, 'error', 'blocks');
+	}
+	 
+	return true;
 }
