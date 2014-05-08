87c5764c058ed3ec70a3728e6cc92d8354dc87ab
alexlittle <alex@alexlittle.net>
2014-05-08 18:32:24 +0200

    CLOSED - task 77: Not saving the show feedback option for pre-test (in step 1) https://github.com/DigitalCampus/moodle-block_oppia_mobile_export/issues/issues/issue/77

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014041001;
+$plugin->version = 2014050801;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
