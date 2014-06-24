bdcf75c2cdf4b0a12eed18a0b4ea938d288fd12b
alexlittle <alex@alexlittle.net>
2014-06-24 13:25:16 +0200

    new version no

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014062000;
+$plugin->version = 2014062400;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
