42be8b5c27ecc359b96325374306f1cd6d77cdf3
alexlittle <alex@alexlittle.net>
2014-07-02 16:36:48 +0200

    CLOSED - task 89: Extra new lines being exported in course description https://github.com/DigitalCampus/moodle-block_oppia_mobile_export/issues/issues/issue/89

--- export2.php
+++ export2.php
@@ -125,13 +125,13 @@ $summary = extractLangs($course->summary);
 if(is_array($summary) && count($summary)>0){
 	foreach($summary as $l=>$s){
 		$temp = $xmlDoc->createElement("description");
-		$temp->appendChild($xmlDoc->createTextNode(strip_tags($s)));
+		$temp->appendChild($xmlDoc->createTextNode(trim(strip_tags($s))));
 		$temp->appendChild($xmlDoc->createAttribute("lang"))->appendChild($xmlDoc->createTextNode($l));
 		$meta->appendChild($temp);
 	}
 } else {;
 	$temp = $xmlDoc->createElement("description");
-	$temp->appendChild($xmlDoc->createTextNode(strip_tags($course->summary)));
+	$temp->appendChild($xmlDoc->createTextNode(trim(strip_tags($course->summary))));
 	$temp->appendChild($xmlDoc->createAttribute("lang"))->appendChild($xmlDoc->createTextNode($DEFAULT_LANG));
 	$meta->appendChild($temp);
 }
