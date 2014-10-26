06d9b3f382ea7987b8406c9a7c0a35cca37df0fe
alexlittle <alex@alexlittle.net>
2014-10-26 12:16:41 +0100

    update javascript for questions

--- js/oppia.js
+++ js/oppia.js
@@ -1,7 +1,6 @@
 
 $( document ).ready(function() {
 	$('[name=reveal]').each(function(i){
-		$('#answer'+$(this).attr('id')).toggle();
 		$(this).click(function() {
 			$('#answer'+$(this).attr('id')).toggle("blind", 1000 );
 			$(this).hide();
