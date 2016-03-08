2671f2076e468c66b238a54788e28acb18fcb410
Joseba S <jjoseba@gmail.com>
2016-03-08 19:11:25 +0100

    #135: Updated DB 'value' field length, to support longer tags (it had only 20 characters before)

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016022400;
+$plugin->version = 2016030800;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
