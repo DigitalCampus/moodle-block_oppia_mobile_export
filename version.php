d4c42911a6f26ed3401079795f7b3c6ba7ff755b
Joseba S <jjoseba@gmail.com>
2016-10-11 18:56:18 +0200

    #142: Updated block version

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2016082600;
+$plugin->version = 2016101100;
 $plugin->requires = 2012120300;
 $plugin->component = 'block_oppia_mobile_export';
