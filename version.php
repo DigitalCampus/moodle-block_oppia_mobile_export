e52e4112c6cc2ab4990f65a6a9ca80dbd8ef10e1
Alex Little <alex@digital-campus.org>
2020-01-30 16:22:50 +0200

    OPPIA-201 #close now fixed

--- version.php
+++ version.php
@@ -17,6 +17,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2019103100;
+$plugin->version = 2020013000;
 $plugin->requires = 2016052300;
 $plugin->component = 'block_oppia_mobile_export';
