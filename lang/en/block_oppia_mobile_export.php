854a54c568b7892796d96cc67af613322001c894
Alex Little <alex@digital-campus.org>
2020-12-23 11:21:24 +0100

    new release branch

--- lang/en/block_oppia_mobile_export.php
+++ lang/en/block_oppia_mobile_export.php
@@ -26,7 +26,7 @@
 
 
 $string['pluginname'] = 'Oppia Mobile Export';
-$string['release'] = 'v1.2.3';
+$string['release'] = 'v1.2.4';
 
 
 $string['oppia_mobile_export:addinstance'] = 'Add a new Oppia export block';
