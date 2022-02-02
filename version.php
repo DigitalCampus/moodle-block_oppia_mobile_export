f28949a2b451f119344a84128d8942bffbde8dc2
Alex Little <alex@digital-campus.org>
2022-02-02 16:59:51 +0200

    update version

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin = new StdClass;
-$plugin->version = 2022020200;
+$plugin->version = 2022020201;
 $plugin->requires = 2016052300;
 $plugin->component = 'block_oppia_mobile_export';
 $plugin->release = "v1.3.0";
