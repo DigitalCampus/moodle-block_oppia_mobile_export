d999257fbca3b28b46bc3faf7ffd4a7dcf178e53
Alex Little <alex@digital-campus.org>
2022-04-26 15:06:02 +0300

    next release branch

--- version.php
+++ version.php
@@ -21,5 +21,5 @@ $plugin = new StdClass;
 $plugin->version = 2022042500;
 $plugin->requires = 2016052300;
 $plugin->component = 'block_oppia_mobile_export';
-$plugin->release = "v1.3.2";
+$plugin->release = "v1.3.3";
 $plugin->cron = 0; // Cron interval in seconds. 0 means no cron.
