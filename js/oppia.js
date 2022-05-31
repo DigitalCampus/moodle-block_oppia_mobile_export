d61d94c32ccae4afa5aa232e270a7e84f3a17952
Joseba S <jjoseba@gmail.com>
2022-05-31 17:16:37 +0200

    oppia-832: Added inline-question functionality

--- js/oppia.js
+++ js/oppia.js
@@ -1,13 +1,34 @@
-
 $( document ).ready(function() {
 	$('[name=reveal]').each(function(i){
-		var revealBtn = $(this).addClass('showmore revealed');
+		var revealSection = $(this).addClass('showmore revealed');
 		var target = $('#answer'+$(this).attr('id'));
-		target.addClass('showmore').show();
-		revealBtn.click(function() {
+
+		function revealContent(){
 			target.addClass('revealed');
-			revealBtn.removeClass('revealed');
-		});
+			revealSection.removeClass('revealed');
 		}
-	);
+
+		target.addClass('showmore').show();
+		if (revealSection.has('button').length > 0){
+			
+			revealSection.find('button').on('click', function(){
+				inputValue = revealSection.find('input').eq(0).val();
+				if (inputValue != null && inputValue != ''){ 
+					revealContent(); 
+				}
+				else{
+					var errorMsg = revealSection.find('.error-msg');
+					if (errorMsg.length == 0){
+						errorMsg = $('<div class="error-msg" style="display:none;">You have to enter some text.</div>');
+						revealSection.append(errorMsg);
+					}
+					errorMsg.fadeIn();
+				}
+			});
+		}		
+		else{
+			revealSection.on('click', revealContent);	
+		}
+		
+	});
 });
