43dbed700d50445f9332c3ddd7ecae2d0e46e25e
Joseba S <jjoseba@gmail.com>
2022-09-29 16:52:53 +0200

    Added console script to execute the populate_digests migration

---
+++ migrations/20220929_migrate_script.php
@@ -0,0 +1,19 @@
+<?php 
+
+define('CLI_SCRIPT', true);
+
+require_once(dirname(__FILE__) . '/../constants.php');
+require_once(dirname(__FILE__) . '/../migrations/populate_digests.php');
+
+global $CFG;
+$CFG->block_oppia_mobile_export_debug = false;
+
+$starttime = microtime(true);
+
+echo 'Starting populate digest function';
+populate_digests_published_courses(null, false);
+
+$timediff = $starttime - microtime(true);
+echo 'Completed in ' . $timediff . ' seconds.';
+
+?>
