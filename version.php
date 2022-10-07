b620dfee38436d343a72442bf76d92b25326e9aa
Joseba S <jjoseba@gmail.com>
2022-10-07 13:15:54 +0200

    Update release name

--- version.php
+++ version.php
@@ -21,5 +21,5 @@ $plugin = new StdClass;
 $plugin->version = 2022093000;
 $plugin->requires = 2016052300;
 $plugin->component = 'block_oppia_mobile_export';
-$plugin->release = "v1.3.4";
+$plugin->release = "v1.3.5b";
 $plugin->cron = 0; // Cron interval in seconds. 0 means no cron.
