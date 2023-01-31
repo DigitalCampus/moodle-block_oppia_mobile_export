d426c0ee24cae3188e35b6b48dbaf3d545476374
Alberto Moreta <albertomoreta70@gmail.com>
2023-01-31 14:41:00 +0100

    update version no

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin = new StdClass;
-$plugin->version = 2023012400;
+$plugin->version = 2023013100;
 $plugin->requires = 2022041900;
 $plugin->component = 'block_oppia_mobile_export';
 $plugin->release = "v1.4.1";
