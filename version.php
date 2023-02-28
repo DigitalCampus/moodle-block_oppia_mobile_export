f91bec64d1787dcf998d7e55823c2470f348d948
Alberto Moreta <albertomoreta70@gmail.com>
2023-02-28 12:20:02 +0100

    Next release branch

--- version.php
+++ version.php
@@ -21,5 +21,5 @@ $plugin = new StdClass;
 $plugin->version = 2023022800;
 $plugin->requires = 2022041900;
 $plugin->component = 'block_oppia_mobile_export';
-$plugin->release = "v1.4.2";
+$plugin->release = "v1.4.3";
 $plugin->cron = 0; // Cron interval in seconds. 0 means no cron.
